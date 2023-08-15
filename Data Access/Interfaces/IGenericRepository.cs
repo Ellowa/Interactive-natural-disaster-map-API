a5381ab5ade967ea2ce8a1de44af4c9d6e7eee4e
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-15 20:03:56 +0300

    Added Interface IGenericRepository

---
+++ Data Access/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace Data_Access.Interfaces
+{
+    internal interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<IQueryable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes);
+
+        Task<TEntity> GetByIdAsync(int id, params Expression<Func<TEntity, object>>[] includes);
+
+        Task AddAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task DeleteByIdAsync(int id);
+
+        void Update(TEntity entity);
+    }
+}
