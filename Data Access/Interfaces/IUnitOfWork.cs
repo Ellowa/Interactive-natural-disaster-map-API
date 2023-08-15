0fc606d45fc3ef4c815d75e511a8015d11cc9a7c
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-15 20:04:10 +0300

    Added Interface IUnitOfWork

---
+++ Data Access/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace Data_Access.Interfaces
+{
+    internal interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class, new();
+
+        Task SaveAsync();
+    }
+}
