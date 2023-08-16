f6ea973a4cfc9eb6161f7084f18c54e36672db09
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-16 20:01:21 +0300

    Added BaseRepository

---
+++ Data Access/Repositories/BaseRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq.Expressions;
+using Data_Access.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Access.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class, new()
+    {
+        protected readonly ApplicationDbContext Context;
+        protected readonly DbSet<TEntity> DbSet;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await DbSet.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await DbSet.FindAsync(id);
+            if (entity != null)
+                DbSet.Remove(entity);
+        }
+
+        public async Task<IQueryable<TEntity>> GetAllAsync(params Expression<Func<TEntity, object>>[] includes)
+        {
+            IQueryable<TEntity> query = DbSet;
+
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return query;
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
