ab82691987784d2f9c1222df55c8952e6085b1aa
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-16 20:02:35 +0300

    Added EventsCollectionRepository

---
+++ Data Access/Repositories/EventsCollectionRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using Data_Access.Entities;
+using Data_Access.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data_Access.Repositories
+{
+    public class EventsCollectionRepository : BaseRepository<EventsCollection>, IEventsCollectionRepository
+    {
+        public EventsCollectionRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<EventsCollection?> GetByEventIdAsync(int eventId,
+            params Expression<Func<EventsCollection, object>>[] includes)
+        {
+            IQueryable<EventsCollection> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(e => e.EventId == eventId);
+        }
+
+        public async Task<EventsCollection?> GetByUserIdAsync(int userId,
+            params Expression<Func<EventsCollection, object>>[] includes)
+        {
+            IQueryable<EventsCollection> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(e => e.UserId == userId);
+        }
+
+        public async Task<EventsCollection?> GetByCollectionName(string collectionName,
+            params Expression<Func<EventsCollection, object>>[] includes)
+        {
+            IQueryable<EventsCollection> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(e => e.EventsCollectionInfo.CollectionName == collectionName);
+        }
+    }
+}
