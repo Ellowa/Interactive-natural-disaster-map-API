55681bde1d9c7a02ec624634f022729b3ef1b82a
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-21 17:45:39 +0300

    Deleted unnecessary usings in UnitOfWork

--- Data Access/UnitOfWork.cs
+++ Data Access/UnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
-using Data_Access.Entities;
+﻿using Data_Access.Entities;
 using Data_Access.Interfaces;
 using Data_Access.Repositories;
 
