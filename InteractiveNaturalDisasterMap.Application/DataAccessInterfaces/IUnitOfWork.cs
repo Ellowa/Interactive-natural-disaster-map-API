e3f01a5401c6ed2456e5e51ffce90af42a5f6a95
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-24 20:35:29 +0300

    Update UnitOfWork

--- InteractiveNaturalDisasterMap.Application/DataAccessInterfaces/IUnitOfWork.cs
+++ InteractiveNaturalDisasterMap.Application/DataAccessInterfaces/IUnitOfWork.cs
@@ -1,8 +1,19 @@
-﻿namespace InteractiveNaturalDisasterMap.Application.DataAccessInterfaces
+﻿using InteractiveNaturalDisasterMap.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.DataAccessInterfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IBaseRepository<TEntity> GetRepository<TEntity>() where TEntity : class, new();
+        IGenericBaseEntityRepository<EventCategory> EventCategoryRepository { get; }
+        IGenericBaseEntityRepository<NaturalDisasterEvent> NaturalDisasterEventRepository { get; }
+        IGenericBaseEntityRepository<EventCoordinate> EventCoordinateRepository { get; }
+        IGenericBaseEntityRepository<EventSource> EventSourceRepository { get; }
+        IGenericBaseEntityRepository<MagnitudeUnit> MagnitudeUnitRepository { get; }
+        IEventsCollectionRepository EventsCollectionRepository { get; }
+        IGenericBaseEntityRepository<EventsCollectionInfo> EventsCollectionInfoRepository { get; }
+        IGenericBaseEntityRepository<User> UserRepository { get; }
+        IGenericBaseEntityRepository<UserRole> UserRoleRepository { get; }
+        IUnconfirmedEventRepository UnconfirmedEventRepository { get; }
 
         Task SaveAsync();
     }
--- InteractiveNaturalDisasterMap.DataAccess.PostgreSql/UnitOfWork.cs
+++ InteractiveNaturalDisasterMap.DataAccess.PostgreSql/UnitOfWork.cs
@@ -8,82 +8,58 @@ namespace InteractiveNaturalDisasterMap.DataAccess.PostgreSql
     {
         private readonly InteractiveNaturalDisasterMapDbContext _context;
 
-        private IBaseRepository<NaturalDisasterEvent> _naturalDisasterEventRepository;
-        private IBaseRepository<EventCategory> _eventCategoryRepository;
-        private IBaseRepository<EventCoordinate> _eventCoordinateRepository;
-        private IBaseRepository<EventSource> _eventSourceRepository;
-        private IBaseRepository<MagnitudeUnit> _magnitudeUnitRepository;
-        private IBaseRepository<EventsCollection> _eventsCollectionRepository;
-        private IBaseRepository<EventsCollectionInfo> _eventsCollectionInfoRepository;
-        private IBaseRepository<User> _useRepository;
-        private IBaseRepository<UserRole> _userRoleRepository;
-        private IBaseRepository<UnconfirmedEvent> _unconfirmedEventRepository;
+        private IGenericBaseEntityRepository<NaturalDisasterEvent> _naturalDisasterEventRepository;
+        private IGenericBaseEntityRepository<EventCategory> _eventCategoryRepository;
+        private IGenericBaseEntityRepository<EventCoordinate> _eventCoordinateRepository;
+        private IGenericBaseEntityRepository<EventSource> _eventSourceRepository;
+        private IGenericBaseEntityRepository<MagnitudeUnit> _magnitudeUnitRepository;
+        private IEventsCollectionRepository _eventsCollectionRepository;
+        private IGenericBaseEntityRepository<EventsCollectionInfo> _eventsCollectionInfoRepository;
+        private IGenericBaseEntityRepository<User> _userRepository;
+        private IGenericBaseEntityRepository<UserRole> _userRoleRepository;
+        private IUnconfirmedEventRepository _unconfirmedEventRepository;
 
-        private bool _disposed = false;
 
         public UnitOfWork(InteractiveNaturalDisasterMapDbContext context)
         {
             this._context = context;
-            //Initialization of repositories
-            _naturalDisasterEventRepository = new GenericBaseEntityRepository<NaturalDisasterEvent>(_context);
-            _eventCategoryRepository = new GenericBaseEntityRepository<EventCategory>(_context);
-            _eventCoordinateRepository = new GenericBaseEntityRepository<EventCoordinate>(_context);
-            _eventSourceRepository = new GenericBaseEntityRepository<EventSource>(_context);
-            _magnitudeUnitRepository = new GenericBaseEntityRepository<MagnitudeUnit>(_context);
-            _eventsCollectionRepository = new EventsCollectionRepository(_context);
-            _eventsCollectionInfoRepository = new GenericBaseEntityRepository<EventsCollectionInfo>(_context);
-            _useRepository = new GenericBaseEntityRepository<User>(_context);
-            _userRoleRepository = new GenericBaseEntityRepository<UserRole>(_context);
-            _unconfirmedEventRepository = new UnconfirmedEventRepository(_context);
         }
+
+        public IGenericBaseEntityRepository<EventCategory> EventCategoryRepository =>
+            _eventCategoryRepository ??= new GenericBaseEntityRepository<EventCategory>(_context);
+
+        public IGenericBaseEntityRepository<NaturalDisasterEvent> NaturalDisasterEventRepository =>
+            _naturalDisasterEventRepository ??= new GenericBaseEntityRepository<NaturalDisasterEvent>(_context);
+
+        public IGenericBaseEntityRepository<EventCoordinate> EventCoordinateRepository =>
+            _eventCoordinateRepository ??= new GenericBaseEntityRepository<EventCoordinate>(_context);
+
+        public IGenericBaseEntityRepository<EventSource> EventSourceRepository =>
+            _eventSourceRepository ??= new GenericBaseEntityRepository<EventSource>(_context);
+
+        public IGenericBaseEntityRepository<MagnitudeUnit> MagnitudeUnitRepository =>
+            _magnitudeUnitRepository ??= new GenericBaseEntityRepository<MagnitudeUnit>(_context);
+
+        public IEventsCollectionRepository EventsCollectionRepository =>
+            _eventsCollectionRepository ??= new EventsCollectionRepository(_context);
+
+        public IGenericBaseEntityRepository<EventsCollectionInfo> EventsCollectionInfoRepository =>
+            _eventsCollectionInfoRepository ??= new GenericBaseEntityRepository<EventsCollectionInfo>(_context);
+
+        public IGenericBaseEntityRepository<User> UserRepository =>
+            _userRepository ??= new GenericBaseEntityRepository<User>(_context);
+
+        public IGenericBaseEntityRepository<UserRole> UserRoleRepository =>
+            _userRoleRepository ??= new GenericBaseEntityRepository<UserRole>(_context);
+
+        public IUnconfirmedEventRepository UnconfirmedEventRepository =>
+            _unconfirmedEventRepository ??= new UnconfirmedEventRepository(_context);
+
 
         public void Dispose()
         {
-            Dispose(true);
+            _context.Dispose();
             GC.SuppressFinalize(this);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (disposing)
-                {
-                    _context.Dispose();
-                }
-                _disposed = true;
-            }
-        }
-
-        public IBaseRepository<TEntity> GetRepository<TEntity>() where TEntity : class, new()
-        {
-            TEntity entity = new TEntity();
-
-            switch (entity)
-            {
-                case NaturalDisasterEvent _:
-                    return (_naturalDisasterEventRepository as IBaseRepository<TEntity>)!;
-                case EventCategory _:
-                    return (_eventCategoryRepository as IBaseRepository<TEntity>)!;
-                case EventCoordinate _:
-                    return (_eventCoordinateRepository as IBaseRepository<TEntity>)!;
-                case EventSource _:
-                    return (_eventSourceRepository as IBaseRepository<TEntity>)!;
-                case MagnitudeUnit _:
-                    return (_magnitudeUnitRepository as IBaseRepository<TEntity>)!;
-                case EventsCollectionRepository _:
-                    return (_eventsCollectionRepository as IBaseRepository<TEntity>)!;
-                case EventsCollectionInfo _:
-                    return (_eventsCollectionInfoRepository as IBaseRepository<TEntity>)!;
-                case User _:
-                    return (_useRepository as IBaseRepository<TEntity>)!;
-                case UserRole _:
-                    return (_userRoleRepository as IBaseRepository<TEntity>)!;
-                case UnconfirmedEvent _:
-                    return (_unconfirmedEventRepository as IBaseRepository<TEntity>)!;
-                default:
-                    throw new NotImplementedException();
-            }
         }
 
         public Task SaveAsync()
