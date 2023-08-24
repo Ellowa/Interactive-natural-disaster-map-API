6399005844be775e1fbb81866734d22c9de29db3
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-24 18:11:09 +0300

    Add UnconfirmedEventRepository

---
+++ InteractiveNaturalDisasterMap.Application/DataAccessInterfaces/IUnconfirmedEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using InteractiveNaturalDisasterMap.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.DataAccessInterfaces
+{
+    public interface IUnconfirmedEventRepository : IBaseRepository<UnconfirmedEvent>
+    {
+        Task<IReadOnlyList<UnconfirmedEvent>> GetByUserId(int userId,
+            params Expression<Func<UnconfirmedEvent, object>>[] includes);
+
+        Task<UnconfirmedEvent?> GetByEventId(int eventId,
+            params Expression<Func<UnconfirmedEvent, object>>[] includes);
+    }
+}
---
+++ InteractiveNaturalDisasterMap.DataAccess.PostgreSql/Repositories/UnconfirmedEventRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq.Expressions;
+using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InteractiveNaturalDisasterMap.DataAccess.PostgreSql.Repositories
+{
+    public class UnconfirmedEventRepository : BaseRepository<UnconfirmedEvent>, IUnconfirmedEventRepository
+    {
+        public UnconfirmedEventRepository(InteractiveNaturalDisasterMapDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IReadOnlyList<UnconfirmedEvent>> GetByUserId(int userId, params Expression<Func<UnconfirmedEvent, object>>[] includes)
+        {
+            IQueryable<UnconfirmedEvent> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.Where(e => e.UserId == userId).ToListAsync();
+        }
+
+        public async Task<UnconfirmedEvent?> GetByEventId(int eventId, params Expression<Func<UnconfirmedEvent, object>>[] includes)
+        {
+            IQueryable<UnconfirmedEvent> query = DbSet;
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            return await query.FirstOrDefaultAsync(e => e.EventId == eventId);
+        }
+    }
+}
