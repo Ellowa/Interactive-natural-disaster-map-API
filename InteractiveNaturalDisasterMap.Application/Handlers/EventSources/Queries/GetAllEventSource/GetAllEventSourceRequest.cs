046eb32442de1befd081d757df9204dc167005b1
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-26 17:56:27 +0300

    Add EventSource getAll query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventSources/Queries/GetAllEventSource/GetAllEventSourceRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventSources.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventSources.Queries.GetAllEventSource
+{
+    public class GetAllEventSourceRequest : IRequest<IList<EventSourceDto>>
+    {
+    }
+}
