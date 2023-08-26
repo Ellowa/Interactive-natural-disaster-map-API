4021009cbfa2ef24d9954db25f51281b4f4dca5f
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-26 17:56:51 +0300

    Add EventSource getById query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventSources/Queries/GetByIdEventSource/GetByIdEventSourceHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventSources.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventSources.Queries.GetByIdEventSource
+{
+    public class GetByIdEventSourceHandler : IRequestHandler<GetByIdEventSourceRequest, EventSourceDto>
+    {
+        private readonly IGenericBaseEntityRepository<EventSource> _eventSourceRepository;
+
+        public GetByIdEventSourceHandler(IUnitOfWork unitOfWork)
+        {
+            _eventSourceRepository = unitOfWork.EventSourceRepository;
+        }
+
+        public async Task<EventSourceDto> Handle(GetByIdEventSourceRequest request, CancellationToken cancellationToken)
+        {
+            var eventSource = await _eventSourceRepository.GetByIdAsync(request.GetByIdEventSourceDto.Id, cancellationToken) 
+                                ?? throw new NotFoundException(nameof(EventSource), request.GetByIdEventSourceDto.Id);
+
+            return new EventSourceDto(eventSource);
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventSources/Queries/GetByIdEventSource/GetByIdEventSourceRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventSources.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventSources.Queries.GetByIdEventSource
+{
+    public class GetByIdEventSourceRequest : IRequest<EventSourceDto>
+    {
+        public GetByIdEventSourceDto GetByIdEventSourceDto { get; set; } = null!;
+    }
+}
