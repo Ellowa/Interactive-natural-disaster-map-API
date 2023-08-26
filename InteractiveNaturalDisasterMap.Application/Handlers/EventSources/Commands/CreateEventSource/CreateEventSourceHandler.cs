13a832a26573596e98033f091943293bfb952ec8
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-26 17:55:14 +0300

    Add EventSource create command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventSources/Commands/CreateEventSource/CreateEventSourceHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventSources.Commands.CreateEventSource
+{
+    public class CreateEventSourceHandler : IRequestHandler<CreateEventSourceRequest, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<EventSource> _eventSourceRepository;
+
+        public CreateEventSourceHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _eventSourceRepository = unitOfWork.EventSourceRepository;
+        }
+
+        public async Task<int> Handle(CreateEventSourceRequest request, CancellationToken cancellationToken)
+        {
+            var entity = request.CreateEventSourceDto.Map();
+            await _eventSourceRepository.AddAsync(entity, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
