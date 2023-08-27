5703bd17e96523b93ad6b8f0093b552b83fa4988
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-27 17:35:07 +0300

    Add createMagnitudeUnit command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/MagnitudeUnits/Commands/CreateMagnitudeUnit/CreateMagnitudeUnitHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.MagnitudeUnits.Commands.CreateMagnitudeUnit
+{
+    public class CreateMagnitudeUnitHandler : IRequestHandler<CreateMagnitudeUnitRequest, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<MagnitudeUnit> _magnitudeUnitRepository;
+
+        public CreateMagnitudeUnitHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _magnitudeUnitRepository = unitOfWork.MagnitudeUnitRepository;
+        }
+
+        public async Task<int> Handle(CreateMagnitudeUnitRequest request, CancellationToken cancellationToken)
+        {
+            var entity = request.CreateMagnitudeUnitDto.Map();
+            await _magnitudeUnitRepository.AddAsync(entity, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/MagnitudeUnits/Commands/CreateMagnitudeUnit/CreateMagnitudeUnitRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.MagnitudeUnits.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.MagnitudeUnits.Commands.CreateMagnitudeUnit
+{
+    public class CreateMagnitudeUnitRequest : IRequest<int>
+    {
+        public CreateMagnitudeUnitDto CreateMagnitudeUnitDto { get; set; } = null!;
+    }
+}
