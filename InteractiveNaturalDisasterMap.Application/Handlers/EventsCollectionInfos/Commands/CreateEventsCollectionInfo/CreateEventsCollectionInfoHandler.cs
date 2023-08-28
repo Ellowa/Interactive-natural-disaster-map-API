2451fbbc950fb40822c752d6e9bdec3f925e5480
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 19:17:06 +0300

    Add create eventsCollectionInfo command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollectionInfos/Commands/CreateEventsCollectionInfo/CreateEventsCollectionInfoHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.Commands.CreateEventsCollectionInfo
+{
+    public class CreateEventsCollectionInfoHandler : IRequestHandler<CreateEventsCollectionInfoRequest, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<EventsCollectionInfo> _eventsCollectionInfoRepository;
+
+        public CreateEventsCollectionInfoHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _eventsCollectionInfoRepository = unitOfWork.EventsCollectionInfoRepository;
+        }
+
+        public async Task<int> Handle(CreateEventsCollectionInfoRequest request, CancellationToken cancellationToken)
+        {
+            var entity = request.CreateEventsCollectionInfoDto.Map();
+            await _eventsCollectionInfoRepository.AddAsync(entity, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
