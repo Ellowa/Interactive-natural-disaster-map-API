a99cc7e830317ef4f4386c021658089e07e486f8
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 18:25:00 +0300

    Add update hazardUnit command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Commands/UpdateEventHazardUnit/UpdateEventHazardUnitHandler.cs
@@ -0,0 +1,28 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Commands.UpdateEventHazardUnit
+{
+    public class UpdateEventHazardUnitHandler : IRequestHandler<UpdateEventHazardUnitRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<EventHazardUnit> _eventHazardUnitRepository;
+
+        public UpdateEventHazardUnitHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _eventHazardUnitRepository = unitOfWork.EventHazardUnitRepository;
+        }
+
+        public async Task Handle(UpdateEventHazardUnitRequest request, CancellationToken cancellationToken)
+        {
+            if (await _eventHazardUnitRepository.GetByIdAsync(request.UpdateEventHazardUnitDto.Id, cancellationToken) == null)
+                throw new NotFoundException(nameof(EventHazardUnit), request.UpdateEventHazardUnitDto.Id);
+
+            _eventHazardUnitRepository.Update(await request.UpdateEventHazardUnitDto.MapAsync(_unitOfWork, cancellationToken));
+            await _unitOfWork.SaveAsync(cancellationToken);
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Commands/UpdateEventHazardUnit/UpdateEventHazardUnitRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Commands.UpdateEventHazardUnit
+{
+    public class UpdateEventHazardUnitRequest : IRequest
+    {
+        public UpdateEventHazardUnitDto UpdateEventHazardUnitDto { get; set; } = null!;
+    }
+}
