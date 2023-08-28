f1a568870ef20bf67bfdad92619c6ed53f06582b
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 18:25:41 +0300

    Add getById hazardUnit query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Queries/GetByIdEventHazardUnit/GetByIdMagnitudeUnitRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Queries.GetByIdEventHazardUnit
+{
+    public class GetByIdEventHazardUnitRequest : IRequest<EventHazardUnitDto>
+    {
+        public GetByIdEventHazardUnitDto GetByIdEventHazardUnitDto { get; set; } = null!;
+    }
+}
