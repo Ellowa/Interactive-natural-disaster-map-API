fe6ebc6d8036f85af8adac9de9eb8b0a3eeddee8
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 19:17:37 +0300

    Add getAll eventsCollectionInfo query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollectionInfos/Queries/GetAllEventsCollectionInfo/GetAllEventsCollectionInfoRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.Queries.GetAllEventsCollectionInfo
+{
+    public class GetAllEventsCollectionInfoRequest : IRequest<IList<EventsCollectionInfoDto>>
+    {
+    }
+}
