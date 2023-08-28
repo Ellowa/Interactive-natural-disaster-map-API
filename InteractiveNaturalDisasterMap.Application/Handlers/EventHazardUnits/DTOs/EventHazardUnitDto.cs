0e3526b59228d8273e51a0e1135a8c8c59f8589a
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 18:24:40 +0300

    Add hazardUnit DTOs

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/DTOs/EventHazardUnitDto.cs
@@ -0,0 +1,27 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.MagnitudeUnits.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs
+{
+    public class EventHazardUnitDto
+    {
+        public int Id { get; set; }
+
+        public string HazardName { get; set; }
+
+        public string MagnitudeUnitName { get; set; }
+
+        public double? ThresholdValue { get; set; }
+
+        public MagnitudeUnitDto MagnitudeUnitDto { get; set; }
+
+        public EventHazardUnitDto(EventHazardUnit eventHazardUnit)
+        {
+            Id = eventHazardUnit.Id;
+            HazardName = eventHazardUnit.HazardName;
+            ThresholdValue = eventHazardUnit.ThresholdValue;
+            MagnitudeUnitName = eventHazardUnit.MagnitudeUnit.MagnitudeUnitName;
+            MagnitudeUnitDto = new MagnitudeUnitDto(eventHazardUnit.MagnitudeUnit);
+        }
+    }
+}
