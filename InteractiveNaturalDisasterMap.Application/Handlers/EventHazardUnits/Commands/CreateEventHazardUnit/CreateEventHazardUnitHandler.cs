7728bbff9225abb86513275e0e19660399813f6f
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 18:24:08 +0300

    Add create hazardUnit command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Commands/CreateEventHazardUnit/CreateEventHazardUnitHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Commands.CreateEventHazardUnit
+{
+    public class CreateEventHazardUnitHandler : IRequestHandler<CreateEventHazardUnitRequest, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<EventHazardUnit> _eventHazardUnitRepository;
+
+        public CreateEventHazardUnitHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _eventHazardUnitRepository = unitOfWork.EventHazardUnitRepository;
+        }
+
+        public async Task<int> Handle(CreateEventHazardUnitRequest request, CancellationToken cancellationToken)
+        {
+            var entity = await request.CreateEventHazardUnitDto.MapAsync(_unitOfWork, cancellationToken);
+            await _eventHazardUnitRepository.AddAsync(entity, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+            return entity.Id;
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Commands/CreateEventHazardUnit/CreateEventHazardUnitRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Commands.CreateEventHazardUnit
+{
+    public class CreateEventHazardUnitRequest : IRequest<int>
+    {
+        public CreateEventHazardUnitDto CreateEventHazardUnitDto { get; set; } = null!;
+    }
+}
