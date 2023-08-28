0406b7ec2170e0b21a8641fb801ad9aaa02a6bdb
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 18:25:25 +0300

    Add getAll hazardUnit query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Queries/GetAllEventHazardUnit/GetAllEventHazardUnitHandler.cs
@@ -0,0 +1,29 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Queries.GetAllEventHazardUnit
+{
+    public class GetAllEventHazardUnitHandler : IRequestHandler<GetAllEventHazardUnitRequest, IList<EventHazardUnitDto>>
+    {
+        private readonly IGenericBaseEntityRepository<EventHazardUnit> _eventHazardUnitRepository;
+
+        public GetAllEventHazardUnitHandler(IUnitOfWork unitOfWork)
+        {
+            _eventHazardUnitRepository = unitOfWork.EventHazardUnitRepository;
+        }
+
+        public async Task<IList<EventHazardUnitDto>> Handle(GetAllEventHazardUnitRequest request, CancellationToken cancellationToken)
+        {
+            var hazardUnits = await _eventHazardUnitRepository.GetAllAsync(cancellationToken, ehu => ehu.MagnitudeUnit);
+            IList<EventHazardUnitDto> hazardUnitDtos = new List<EventHazardUnitDto>(); 
+            foreach (var hazardUnit in hazardUnits)
+            {
+                hazardUnitDtos.Add(new EventHazardUnitDto(hazardUnit));
+            }
+
+            return hazardUnitDtos;
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventHazardUnits/Queries/GetAllEventHazardUnit/GetAllEventHazardUnitRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventHazardUnits.Queries.GetAllEventHazardUnit
+{
+    public class GetAllEventHazardUnitRequest : IRequest<IList<EventHazardUnitDto>>
+    {
+    }
+}
