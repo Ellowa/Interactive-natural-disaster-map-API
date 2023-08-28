93e978525898cb16bc37356dcc3207b596ee78ee
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 19:16:51 +0300

    Add eventsCollectionInfos DTOs

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollectionInfos/DTOs/UpdateEventsCollectionInfoDto.cs
@@ -0,0 +1,21 @@
+﻿
+using InteractiveNaturalDisasterMap.Domain.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.DTOs
+{
+    public class UpdateEventsCollectionInfoDto
+    {
+        public int Id { get; set; }
+        public string CollectionName { get; set; } = null!;
+
+        public EventsCollectionInfo Map()
+        {
+            EventsCollectionInfo eventsCollectionInfo = new EventsCollectionInfo()
+            {
+                Id = this.Id,
+                CollectionName = this.CollectionName,
+            };
+            return eventsCollectionInfo;
+        }
+    }
+}
