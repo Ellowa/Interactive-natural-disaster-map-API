bed406cadd997590183a01ddcdc355b5539569b6
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-28 20:37:36 +0300

    Add create EventsCollection command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollections/Commands/CreateEventsCollection/CreateEventsCollectionHandler.cs
@@ -0,0 +1,31 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.Commands.CreateEventsCollection
+{
+    public class CreateEventsCollectionHandler : IRequestHandler<CreateEventsCollectionRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEventsCollectionRepository _eventsCollectionRepository;
+
+        public CreateEventsCollectionHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _eventsCollectionRepository = unitOfWork.EventsCollectionRepository;
+        }
+
+        public async Task Handle(CreateEventsCollectionRequest request, CancellationToken cancellationToken)
+        {
+            var collectionInfo = await _unitOfWork.EventsCollectionInfoRepository.GetByIdAsync(request.CreateEventsCollectionDto.CollectionId, cancellationToken)
+                         ?? throw new NotFoundException(nameof(EventsCollectionInfo), request.CreateEventsCollectionDto.CollectionId);
+            if (collectionInfo.UserId != request.CreateEventsCollectionDto.UserId)
+                throw new AuthorizationException(nameof(collectionInfo), request.CreateEventsCollectionDto.UserId);
+
+            var entity = request.CreateEventsCollectionDto.Map();
+            await _eventsCollectionRepository.AddAsync(entity, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollections/Commands/CreateEventsCollection/CreateEventsCollectionRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.Commands.CreateEventsCollection
+{
+    public class CreateEventsCollectionRequest : IRequest
+    {
+        public CreateEventsCollectionDto CreateEventsCollectionDto { get; set; } = null!;
+    }
+}
