2ea8ab9c1dfafe30adfae22d71e1b4250ef8d7f6
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 17:33:19 +0300

    Add getById userRole query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/Queries/GetByIdUserRole/GetByIdUserRoleHandler.cs
@@ -0,0 +1,28 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+using InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.Queries.GetByIdUserRole
+{
+    public class GetByIdUserRoleHandler : IRequestHandler<GetByIdUserRoleRequest, UserRoleDto>
+    {
+        private readonly IGenericBaseEntityRepository<UserRole> _userRoleRepository;
+
+        public GetByIdUserRoleHandler(IUnitOfWork unitOfWork)
+        {
+            _userRoleRepository = unitOfWork.UserRoleRepository;
+        }
+
+        public async Task<UserRoleDto> Handle(GetByIdUserRoleRequest request,
+            CancellationToken cancellationToken)
+        {
+            var userRole =
+                await _userRoleRepository.GetByIdAsync(request.GetByIdUserRoleDto.Id, cancellationToken)
+                ?? throw new NotFoundException(nameof(UserRole), request.GetByIdUserRoleDto.Id);
+
+            return new UserRoleDto(userRole);
+        }
+    }
+}
