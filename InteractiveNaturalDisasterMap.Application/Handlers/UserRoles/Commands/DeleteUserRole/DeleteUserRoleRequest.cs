dcbc2877128c2238d11feaacea8647f82d6061a9
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 17:32:41 +0300

    Add deleteuserRole command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/Commands/DeleteUserRole/DeleteUserRoleRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.Commands.DeleteUserRole
+{
+    public class DeleteUserRoleRequest : IRequest
+    {
+        public DeleteUserRoleDto DeleteUserRoleDto { get; set; } = null!;
+    }
+}
