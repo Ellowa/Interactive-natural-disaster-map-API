9684b60e8dc8d76a16fb89bb5d3f146ddae93774
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 17:32:50 +0300

    Add update userRole command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/Commands/UpdateUserRole/UpdateUserRoleRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.Commands.UpdateUserRole
+{
+    public class UpdateUserRoleRequest : IRequest
+    {
+        public UpdateUserRoleDto UpdateUserRoleDto { get; set; } = null!;
+    }
+}
