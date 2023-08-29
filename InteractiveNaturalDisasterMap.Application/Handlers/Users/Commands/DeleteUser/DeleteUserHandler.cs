eec29a34b0310af8053caf99cc5e3d2785dd2519
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 18:26:11 +0300

    Add delete user command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Commands/DeleteUser/DeleteUserHandler.cs
@@ -0,0 +1,28 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Commands.DeleteUser
+{
+    public class DeleteUserHandler : IRequestHandler<DeleteUserRequest>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericBaseEntityRepository<User> _userRepository;
+
+        public DeleteUserHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = unitOfWork.UserRepository;
+        }
+
+        public async Task Handle(DeleteUserRequest request, CancellationToken cancellationToken)
+        {
+            if(await _userRepository.GetByIdAsync(request.DeleteUserDto.Id, cancellationToken) == null)
+                throw new NotFoundException(nameof(User), request.DeleteUserDto.Id);
+
+            await _userRepository.DeleteByIdAsync(request.DeleteUserDto.Id, cancellationToken);
+            await _unitOfWork.SaveAsync(cancellationToken);
+        }
+    }
+}
