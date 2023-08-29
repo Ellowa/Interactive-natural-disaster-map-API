16d34c18051be9bd444a914cd9ca72bb270323ff
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 19:15:06 +0300

    Add getAll unconfirmedEvent query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UnconfirmedEvents/Queries/GetAllUnconfirmedEvent/GetAllUnconfirmedEventRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.UnconfirmedEvents.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UnconfirmedEvents.Queries.GetAllUnconfirmedEvent
+{
+    public class GetAllUnconfirmedEventRequest : IRequest<IList<UnconfirmedEventDto>>
+    {
+    }
+}
