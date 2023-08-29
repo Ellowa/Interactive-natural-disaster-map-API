4fdbfba03b7c50036fcf9cb455ff48bac838c478
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 18:26:46 +0300

    Add getById user query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Queries/GetByIdUser/GetByIdUserHandler.cs
@@ -0,0 +1,26 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Queries.GetByIdUser
+{
+    public class GetByIdUserHandler : IRequestHandler<GetByIdUserRequest, UserDto>
+    {
+        private readonly IGenericBaseEntityRepository<User> _userRepository;
+
+        public GetByIdUserHandler(IUnitOfWork unitOfWork)
+        {
+            _userRepository = unitOfWork.UserRepository;
+        }
+
+        public async Task<UserDto> Handle(GetByIdUserRequest request, CancellationToken cancellationToken)
+        {
+            var user = await _userRepository.GetByIdAsync(request.GetByIdUserDto.Id, cancellationToken, u => u.Role) 
+                                ?? throw new NotFoundException(nameof(User), request.GetByIdUserDto.Id);
+
+            return new UserDto(user);
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Queries/GetByIdUser/GetByIdUserRequest.cs
@@ -0,0 +1,11 @@
+﻿
+using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Queries.GetByIdUser
+{
+    public class GetByIdUserRequest : IRequest<UserDto>
+    {
+        public GetByIdUserDto GetByIdUserDto { get; set; } = null!;
+    }
+}
