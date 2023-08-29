b5aaa46cfd04236a91d93fd0b0080df56bfb9fc9
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 17:32:21 +0300

    Add userRole DTOs

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/DTOs/CreateUserRoleDto.cs
@@ -0,0 +1,18 @@
+﻿using InteractiveNaturalDisasterMap.Domain.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs
+{
+    public class CreateUserRoleDto
+    {
+        public string RoleName { get; set; } = null!;
+
+        public UserRole Map()
+        {
+            UserRole userRole = new UserRole()
+            {
+                RoleName = this.RoleName,
+            };
+            return userRole;
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/DTOs/GetByIdUserRoleDto.cs
@@ -0,0 +1,7 @@
+﻿namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs
+{
+    public class GetByIdUserRoleDto
+    {
+        public int Id { get; set; }
+    }
+}
