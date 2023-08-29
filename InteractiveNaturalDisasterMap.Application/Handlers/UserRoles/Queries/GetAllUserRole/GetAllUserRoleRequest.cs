1c22cab1428c4a4e6e57463c1ea6644ecb44b71d
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 17:33:06 +0300

    Add getAll userRole query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/UserRoles/Queries/GetAllUserRole/GetAllUserRoleRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.UserRoles.Queries.GetAllUserRole
+{
+    public class GetAllUserRoleRequest : IRequest<IList<UserRoleDto>>
+    {
+    }
+}
