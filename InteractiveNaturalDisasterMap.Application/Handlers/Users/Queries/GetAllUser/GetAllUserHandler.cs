0152fb6250e18e9b49ee3d71d34b7cbcd044ed5b
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-29 18:26:33 +0300

    Add getAll user query

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Queries/GetAllUser/GetAllUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Queries.GetAllUser
+{
+    public class GetAllUserHandler : IRequestHandler<GetAllUserRequest, IList<UserDto>>
+    {
+        private readonly IGenericBaseEntityRepository<User> _userRepository;
+
+        public GetAllUserHandler(IUnitOfWork unitOfWork)
+        {
+            _userRepository = unitOfWork.UserRepository;
+        }
+
+        public async Task<IList<UserDto>> Handle(GetAllUserRequest request, CancellationToken cancellationToken)
+        {
+            var users = await _userRepository.GetAllAsync(cancellationToken, u=> u.Role);
+            IList<UserDto> userDtos = new List<UserDto>(); 
+            foreach (var user in users)
+            {
+                userDtos.Add(new UserDto(user));
+            }
+
+            return userDtos;
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Queries/GetAllUser/GetAllUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Queries.GetAllUser
+{
+    public class GetAllUserRequest : IRequest<IList<UserDto>>
+    {
+    }
+}
