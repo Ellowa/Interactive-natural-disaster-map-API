4b82671917c90a2e5cbec134b462024ffa609cb1
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-30 19:06:36 +0300

    Add create NaturalDisasterEvent command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/NaturalDisasterEvents/Commands/CreateNaturalDisasterEvent/CreateNaturalDisasterEventRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.Commands.CreateNaturalDisasterEvent
+{
+    public class CreateNaturalDisasterEventRequest : IRequest<int>
+    {
+        public CreateNaturalDisasterEventDto CreateNaturalDisasterEventDto { get; set; } = null!;
+    }
+}
