709a11235cbda861a4bda243db1ef843e8adf79e
Roman Yeliukhin <ellowa123321@gmail.com>
2023-08-31 19:26:02 +0300

    Update EventsCollectionRepository remove unnecessary methods

--- InteractiveNaturalDisasterMap.Application/DataAccessInterfaces/IEventsCollectionRepository.cs
+++ InteractiveNaturalDisasterMap.Application/DataAccessInterfaces/IEventsCollectionRepository.cs
@@ -5,14 +5,6 @@ namespace InteractiveNaturalDisasterMap.Application.DataAccessInterfaces
 {
     public interface IEventsCollectionRepository : IBaseRepository<EventsCollection>
     {
-        Task<IReadOnlyList<EventsCollection>> GetByEventIdAsync(int eventId,
-            CancellationToken cancellationToken,
-            params Expression<Func<EventsCollection, object>>[] includes);
-
-        Task<IReadOnlyList<EventsCollection>> GetByUserIdAsync(int userId,
-            CancellationToken cancellationToken,
-            params Expression<Func<EventsCollection, object>>[] includes);
-
         Task<EventsCollection?> GetByCollectionName(string collectionName,
             CancellationToken cancellationToken,
             params Expression<Func<EventsCollection, object>>[] includes);
--- InteractiveNaturalDisasterMap.DataAccess.PostgreSql/Repositories/EventsCollectionRepository.cs
+++ InteractiveNaturalDisasterMap.DataAccess.PostgreSql/Repositories/EventsCollectionRepository.cs
@@ -11,33 +11,6 @@ namespace InteractiveNaturalDisasterMap.DataAccess.PostgreSql.Repositories
         {
         }
 
-        public async Task<IReadOnlyList<EventsCollection>> GetByEventIdAsync(int eventId,
-            CancellationToken cancellationToken,
-            params Expression<Func<EventsCollection, object>>[] includes)
-        {
-            IQueryable<EventsCollection> query = DbSet;
-            foreach (var include in includes)
-            {
-                query = query.Include(include);
-            }
-
-            return await query.Where(e => e.EventId == eventId).ToListAsync(cancellationToken);
-        }
-
-        public async Task<IReadOnlyList<EventsCollection>> GetByUserIdAsync(int userId,
-            CancellationToken cancellationToken,
-            params Expression<Func<EventsCollection, object>>[] includes)
-        {
-            IQueryable<EventsCollection> query = DbSet;
-            query = query.Include(x => x.EventsCollectionInfo);
-            foreach (var include in includes)
-            {
-                query = query.Include(include);
-            }
-
-            return await query.Where(e => e.EventsCollectionInfo.UserId == userId).ToListAsync(cancellationToken);
-        }
-
         public async Task<EventsCollection?> GetByCollectionName(string collectionName,
             CancellationToken cancellationToken,
             params Expression<Func<EventsCollection, object>>[] includes)
