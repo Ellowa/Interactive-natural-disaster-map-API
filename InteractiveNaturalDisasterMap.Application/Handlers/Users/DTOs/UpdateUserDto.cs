cb073a5bd0ce00bf8765d4fcca9140da35154f85
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-07 18:53:09 +0300

    Refactor UpdateUser command

--- InteractiveNaturalDisasterMap.Application/Handlers/Users/DTOs/UpdateUserDto.cs
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/DTOs/UpdateUserDto.cs
@@ -1,6 +1,4 @@
-﻿using InteractiveNaturalDisasterMap.Domain.Entities;
-
-namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs
+﻿namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs
 {
     public class UpdateUserDto
     {
@@ -19,23 +17,5 @@ namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs
         public string Login { get; set; } = null!;
 
         public string Password { get; set; } = null!;
-
-        public User Map(byte[] passwordHash, byte[] passwordSalt, int roleId)
-        {
-            User user = new User()
-            {
-                Id = Id,
-                FirstName = this.FirstName,
-                SecondName = this.SecondName,
-                LastName = this.LastName,
-                Email = this.Email,
-                Telegram = this.Telegram,
-                Login = this.Login,
-                PasswordHash = passwordHash,
-                PasswordSalt = passwordSalt,
-                RoleId = roleId,
-            };
-            return user;
-        }
     }
 }
