7e36262475c76bd1107f931d1b78be26ecc5b587
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-13 20:08:05 +0300

    Add EventsCollectionController

---
+++ InteractiveNaturalDisasterMap.Web/Controllers/EventsCollectionController.cs
@@ -0,0 +1,41 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.Commands.AddToEventsCollection;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.Commands.DeleteFromEventsCollection;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveNaturalDisasterMap.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsCollectionController : BaseController
+    {
+        // POST api/EventsCollection
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> AddEventToCollection([FromBody] AddToEventsCollectionDto addToEventsCollectionDto)
+        {
+            var request = new AddToEventsCollectionRequest()
+            {
+                AddToEventsCollectionDto = addToEventsCollectionDto,
+                //Todo userId
+            };
+            await Mediator.Send(request);
+            return NoContent();
+        }
+
+        // DELETE api/EventsCollection/5
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteEventFromCollection([FromBody] DeleteFromEventsCollectionDto deleteFromEventsCollectionDto)
+        {
+            var request = new DeleteFromEventsCollectionRequest()
+            {
+                DeleteFromEventsCollectionDto = deleteFromEventsCollectionDto,
+            };
+            await Mediator.Send(request);
+            return NoContent();
+        }
+    }
+}
