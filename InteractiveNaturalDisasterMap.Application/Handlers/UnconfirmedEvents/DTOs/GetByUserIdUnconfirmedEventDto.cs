c3ad83d2cbc2d410ff381e084ba2e6276780b921
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-13 19:38:01 +0300

    Rebase it to GetAllUnconfirmedEvent

--- InteractiveNaturalDisasterMap.Application/Handlers/UnconfirmedEvents/DTOs/GetByUserIdUnconfirmedEventDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InteractiveNaturalDisasterMap.Application.Handlers.UnconfirmedEvents.DTOs
-{
-    public class GetByUserIdUnconfirmedEventDto
-    {
-        public int UserId { get; set; }
-    }
-}
