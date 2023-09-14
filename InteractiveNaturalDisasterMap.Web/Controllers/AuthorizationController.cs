00c41be5ea999c34783788a0305c25e60ac8ec37
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-14 19:47:11 +0300

    Add AuthorizationController

---
+++ InteractiveNaturalDisasterMap.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,23 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.Users.Commands.LoginUser;
+using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteractiveNaturalDisasterMap.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : BaseController
+    {
+        public async Task<IActionResult> Login([FromBody] LoginUserDto loginUserDto)
+        {
+            var request = new LoginUserRequest()
+            {
+                LoginUserDto = loginUserDto,
+            };
+
+            string token = await Mediator.Send(request);
+
+            return Ok(token);
+        }
+    }
+}
