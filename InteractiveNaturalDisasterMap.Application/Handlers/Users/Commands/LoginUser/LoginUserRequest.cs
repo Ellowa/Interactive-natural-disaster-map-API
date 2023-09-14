acd91ec14e8bbd29dbfb9053d1afc89af91a3257
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-14 19:46:46 +0300

    Add LoginUser command

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/Users/Commands/LoginUser/LoginUserRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.Users.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.Users.Commands.LoginUser
+{
+    public class LoginUserRequest : IRequest<string>
+    {
+        public LoginUserDto LoginUserDto { get; set; } = null!;
+    }
+}
