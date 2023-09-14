785b8b7c5b12855d6c8f022afa996d22485927b6
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-14 20:29:39 +0300

    Implement JwtProvider

---
+++ InteractiveNaturalDisasterMap.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using InteractiveNaturalDisasterMap.Application.InfrastructureInterfaces;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace InteractiveNaturalDisasterMap.Infrastructure.Authentication
+{
+    public sealed class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtProvider(IOptions<JwtOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public string Generate(User user)
+        {
+            var claims = new Claim[]
+            {
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.Name, user.Login),
+            };
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                _jwtOptions.Issuer,
+                _jwtOptions.Audience,
+                claims,
+                null,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
