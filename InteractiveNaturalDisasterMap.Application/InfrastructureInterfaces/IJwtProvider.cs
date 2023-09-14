6007f6c8b6cf4adfc09b2e09017cdee128b1febc
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-14 19:46:08 +0300

    Add IJwtProvider

---
+++ InteractiveNaturalDisasterMap.Application/InfrastructureInterfaces/IJwtProvider.cs
@@ -0,0 +1,9 @@
+﻿using InteractiveNaturalDisasterMap.Domain.Entities;
+
+namespace InteractiveNaturalDisasterMap.Application.InfrastructureInterfaces
+{
+    public interface IJwtProvider
+    {
+        string Generate(User user);
+    }
+}
