1cdaf2ba38f13d05a04aef2588eb2c0fd9e313cb
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-14 21:05:49 +0300

    Add JwtBearer DI registration with access to DI in the JwtBearerOptionsSetup, JwtOptionsSetup

---
+++ InteractiveNaturalDisasterMap.Infrastructure/ServicesRegistration/InfrastructureServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace InteractiveNaturalDisasterMap.Infrastructure.ServicesRegistration
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer();
+            services.ConfigureOptions<JwtOptionsSetup>();
+            services.ConfigureOptions<JwtBearerOptionsSetup>();
+            return services;
+        }
+    }
+}
