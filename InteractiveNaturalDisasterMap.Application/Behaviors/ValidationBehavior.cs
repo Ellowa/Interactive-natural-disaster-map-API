fde5f528b39ae66075b73806dfcea452c75718de
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-16 18:12:58 +0300

    Add ValidationBehavior

---
+++ InteractiveNaturalDisasterMap.Application/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Behaviors
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest, new()
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+
+            var errors = _validators
+                .Select(validator => validator.Validate(request))
+                .Where(validationResult => !validationResult.IsValid)
+                .SelectMany(validationResult => validationResult.Errors)
+                .ToList();
+
+            if (errors.Any())
+            {
+                throw new ValidationException(errors);
+            }
+
+            return await next();
+        }
+    }
+}
