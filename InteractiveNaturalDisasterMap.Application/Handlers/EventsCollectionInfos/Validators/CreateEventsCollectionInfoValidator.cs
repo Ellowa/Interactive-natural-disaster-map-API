fb61805076dd7c6317475e418b11805982930c37
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-16 19:09:28 +0300

    Add EventsCollectionInfo validators

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollectionInfos/Validators/CreateEventsCollectionInfoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.DTOs;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.Validators
+{
+    public sealed class CreateEventsCollectionInfoValidator : AbstractValidator<CreateEventsCollectionInfoDto>
+    {
+        public CreateEventsCollectionInfoValidator()
+        {
+            RuleFor(c => c.CollectionName).NotEmpty();
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollectionInfos/Validators/UpdateEventsCollectionInfoValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.DTOs;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollectionInfos.Validators
+{
+    public sealed class UpdateEventsCollectionInfoValidator : AbstractValidator<UpdateEventsCollectionInfoDto>
+    {
+        public UpdateEventsCollectionInfoValidator()
+        {
+            RuleFor(c => c.Id).NotNull();
+            RuleFor(c => c.CollectionName).NotEmpty();
+        }
+    }
+}
