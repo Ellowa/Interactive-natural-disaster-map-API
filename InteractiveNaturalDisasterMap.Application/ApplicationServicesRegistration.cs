ebc0d48a2729246f62bcc1098b6013119019391e
Roman Yeliukhin <ellowa123321@gmail.com>
2023-09-16 18:21:12 +0300

    DI register ValidationBehavior

--- InteractiveNaturalDisasterMap.Application/ApplicationServicesRegistration.cs
+++ InteractiveNaturalDisasterMap.Application/ApplicationServicesRegistration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using InteractiveNaturalDisasterMap.Application.Behaviors;
 using InteractiveNaturalDisasterMap.Application.Interfaces;
 using InteractiveNaturalDisasterMap.Application.Utilities;
 
@@ -10,7 +11,11 @@ namespace InteractiveNaturalDisasterMap.Application
     {
         public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
         {
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            });
             services.AddScoped<IAuthorizationService, AuthorizationService>();
             return services;
         }
