6afd2cf9c37a6797a9632414d8dc09dd810273e3
Roman Yeliukhin <ellowa123321@gmail.com>
2023-10-08 14:47:56 +0300

    Fix bug when user can add not own unconfirmed event to eventsCollection

--- InteractiveNaturalDisasterMap.Application/Handlers/EventsCollections/Commands/AddToEventsCollection/AddToEventsCollectionHandler.cs
+++ InteractiveNaturalDisasterMap.Application/Handlers/EventsCollections/Commands/AddToEventsCollection/AddToEventsCollectionHandler.cs
@@ -21,10 +21,21 @@ namespace InteractiveNaturalDisasterMap.Application.Handlers.EventsCollections.C
 
         public async Task Handle(AddToEventsCollectionRequest request, CancellationToken cancellationToken)
         {
-            var collectionInfo = await _unitOfWork.EventsCollectionInfoRepository.GetByIdAsync(request.AddToEventsCollectionDto.CollectionId, cancellationToken)
-                         ?? throw new NotFoundException(nameof(EventsCollectionInfo), request.AddToEventsCollectionDto.CollectionId);
+            var collectionInfo =
+                await _unitOfWork.EventsCollectionInfoRepository.GetByIdAsync(request.AddToEventsCollectionDto.CollectionId, cancellationToken)
+                ?? throw new NotFoundException(nameof(EventsCollectionInfo), request.AddToEventsCollectionDto.CollectionId);
 
             await _authorizationService.AuthorizeAsync(request.UserId, collectionInfo.UserId, cancellationToken, collectionInfo, collectionInfo.Id);
+
+            IEnumerable<NaturalDisasterEvent> confirmedNaturalDisasterEvents =
+                await _unitOfWork.NaturalDisasterEventRepository.GetAllAsync(cancellationToken, nde => nde.Confirmed);
+
+            IEnumerable<NaturalDisasterEvent> userUnconfirmedEvents = (await _unitOfWork.UnconfirmedEventRepository
+                    .GetAllAsync(cancellationToken, ue => ue.UserId == request.UserId))
+                .Select(ue => ue.Event);
+            var naturalDisasterEvents = confirmedNaturalDisasterEvents.Union(userUnconfirmedEvents);
+            _ = naturalDisasterEvents.FirstOrDefault(nde => nde.Id == request.AddToEventsCollectionDto.EventId)
+                ?? throw new NotFoundException(nameof(NaturalDisasterEvent), request.AddToEventsCollectionDto.EventId);
 
             var entity = request.AddToEventsCollectionDto.Map();
             await _eventsCollectionRepository.AddAsync(entity, cancellationToken);
