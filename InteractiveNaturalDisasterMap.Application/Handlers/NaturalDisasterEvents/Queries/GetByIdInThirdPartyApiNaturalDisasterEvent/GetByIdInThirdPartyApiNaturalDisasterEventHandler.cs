7a7c4171ebf3e34d97b05746117f2fd2c6a4d668
Roman Yeliukhin <ellowa123321@gmail.com>
2023-10-19 19:27:22 +0300

    Add GetByIdInThirdPartyApiNaturalDisasterEventHandler

---
+++ InteractiveNaturalDisasterMap.Application/Handlers/NaturalDisasterEvents/Queries/GetByIdInThirdPartyApiNaturalDisasterEvent/GetByIdInThirdPartyApiNaturalDisasterEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
+using InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.DTOs;
+using InteractiveNaturalDisasterMap.Domain.Entities;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.Queries.GetByIdInThirdPartyApiNaturalDisasterEvent
+{
+    public class GetByIdInThirdPartyApiNaturalDisasterEventHandler : IRequestHandler<GetByIdInThirdPartyApiNaturalDisasterEventRequest, NaturalDisasterEventDto>
+    {
+        private readonly INaturalDisasterEventRepository _naturalDisasterEventRepository;
+
+        public GetByIdInThirdPartyApiNaturalDisasterEventHandler(IUnitOfWork unitOfWork)
+        {
+            _naturalDisasterEventRepository = unitOfWork.NaturalDisasterEventRepository;
+        }
+
+        public async Task<NaturalDisasterEventDto> Handle(GetByIdInThirdPartyApiNaturalDisasterEventRequest request, CancellationToken cancellationToken)
+        {
+            Expression<Func<NaturalDisasterEvent, bool>> filter = nte => nte.IdInThirdPartyApi == request.GetByIdNaturalDisasterEventDto.Id;
+            var naturalDisasterEvent = (await _naturalDisasterEventRepository.GetAllAsync(cancellationToken, filter))
+                .FirstOrDefault() ?? throw new NotFoundException(nameof(NaturalDisasterEvent), request.GetByIdNaturalDisasterEventDto.Id);
+
+            return new NaturalDisasterEventDto(naturalDisasterEvent);
+        }
+    }
+}
---
+++ InteractiveNaturalDisasterMap.Application/Handlers/NaturalDisasterEvents/Queries/GetByIdInThirdPartyApiNaturalDisasterEvent/GetByIdInThirdPartyApiNaturalDisasterEventRequest.cs
@@ -0,0 +1,10 @@
+﻿using InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.DTOs;
+using MediatR;
+
+namespace InteractiveNaturalDisasterMap.Application.Handlers.NaturalDisasterEvents.Queries.GetByIdInThirdPartyApiNaturalDisasterEvent
+{
+    public class GetByIdInThirdPartyApiNaturalDisasterEventRequest : IRequest<NaturalDisasterEventDto>
+    {
+        public GetByIdInThirdPartyApiNaturalDisasterEventDto GetByIdNaturalDisasterEventDto { get; set; } = null!;
+    }
+}
