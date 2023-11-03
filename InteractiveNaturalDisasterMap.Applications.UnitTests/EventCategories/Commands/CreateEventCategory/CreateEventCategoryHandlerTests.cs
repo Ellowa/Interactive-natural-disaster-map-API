c303ca8181cca79836b6cef1b4e8651b4b081aa3
Roman Yeliukhin <ellowa123321@gmail.com>
2023-11-03 17:48:09 +0200

    Add new ShouldThrowNotFoundException test and update ShouldCreateEventCategoryWithUndefinedMagnitude test

--- InteractiveNaturalDisasterMap.Applications.UnitTests/EventCategories/Commands/CreateEventCategory/CreateEventCategoryHandlerTests.cs
+++ InteractiveNaturalDisasterMap.Applications.UnitTests/EventCategories/Commands/CreateEventCategory/CreateEventCategoryHandlerTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq.Expressions;
+using FluentAssertions;
 using InteractiveNaturalDisasterMap.Application.DataAccessInterfaces;
+using InteractiveNaturalDisasterMap.Application.Exceptions;
 using InteractiveNaturalDisasterMap.Application.Handlers.EventCategories.Commands.CreateEventCategory;
 using InteractiveNaturalDisasterMap.Application.Handlers.EventCategories.DTOs;
 using InteractiveNaturalDisasterMap.Application.Utilities;
@@ -10,15 +12,26 @@ namespace InteractiveNaturalDisasterMap.Applications.UnitTests.EventCategories.C
 {
     public class CreateEventCategoryHandlerTests
     {
+        private Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        private Mock<IGenericBaseEntityRepository<EventCategory>> _eventCategoryRepositoryMock =
+            new Mock<IGenericBaseEntityRepository<EventCategory>>();
+
+        private Mock<IGenericBaseEntityRepository<MagnitudeUnit>> _magnitudeUnitRepositoryMock =
+            new Mock<IGenericBaseEntityRepository<MagnitudeUnit>>();
+
+        [SetUp]
+        protected void SetUp()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _eventCategoryRepositoryMock = new Mock<IGenericBaseEntityRepository<EventCategory>>();
+            _magnitudeUnitRepositoryMock = new Mock<IGenericBaseEntityRepository<MagnitudeUnit>>();
+        }
 
         [Test]
-        public async Task CreateEventCategoryHandlerTests_WhenEventCategoryIsValid_ShouldCreateEventCategoryWithUndefinedMagnitude()
+        public async Task CreateEventCategoryHandlerTests_WhenUndefinedMagnitudeUnitIsExists_ShouldCreateEventCategoryWithUndefinedMagnitude()
         {
             // Arrange
-            var unitOfWork = new Mock<IUnitOfWork>();
-            var eventCategoryRepository = new Mock<IGenericBaseEntityRepository<EventCategory>>();
-            var magnitudeUnitRepository = new Mock<IGenericBaseEntityRepository<MagnitudeUnit>>();
-
             var request = new CreateEventCategoryRequest()
             {
                 CreateEventCategoryDto = new CreateEventCategoryDto { CategoryName = "Test" },
@@ -29,22 +42,60 @@ namespace InteractiveNaturalDisasterMap.Applications.UnitTests.EventCategories.C
                 MagnitudeUnitName = EntityNamesByDefault.DefaultMagnitudeUnit
             };
 
-            eventCategoryRepository.Setup(x => x.AddAsync(It.IsAny<EventCategory>(), It.IsAny<CancellationToken>()));
-            magnitudeUnitRepository.Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<MagnitudeUnit, bool>>>()))
+            _eventCategoryRepositoryMock.Setup(x => x.AddAsync(It.IsAny<EventCategory>(), It.IsAny<CancellationToken>()));
+            _magnitudeUnitRepositoryMock.Setup(x =>
+                    x.GetAllAsync(It.IsAny<CancellationToken>(), mu => mu.MagnitudeUnitName == EntityNamesByDefault.DefaultMagnitudeUnit))
                 .ReturnsAsync(new List<MagnitudeUnit> { undefinedMagnitudeUnit });
 
-            unitOfWork.Setup(x => x.MagnitudeUnitRepository).Returns(magnitudeUnitRepository.Object);
-            unitOfWork.Setup(x => x.EventCategoryRepository).Returns(eventCategoryRepository.Object);
+            _unitOfWorkMock.Setup(x => x.MagnitudeUnitRepository).Returns(_magnitudeUnitRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.EventCategoryRepository).Returns(_eventCategoryRepositoryMock.Object);
 
-            var handler = new CreateEventCategoryHandler(unitOfWork.Object);
+            var handler = new CreateEventCategoryHandler(_unitOfWorkMock.Object);
 
             // Act
-            await handler.Handle(request, CancellationToken.None);
+            var result = await handler.Handle(request, default);
 
             // Assert
-            eventCategoryRepository.Verify(ecr => ecr.AddAsync(It.IsAny<EventCategory>(), CancellationToken.None), Times.Once);
-            magnitudeUnitRepository.Verify(mur => mur.GetAllAsync(It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<MagnitudeUnit, bool>>>()), Times.Once);
-            unitOfWork.Verify(u => u.SaveAsync(CancellationToken.None), Times.Once);
+            _magnitudeUnitRepositoryMock.Verify(
+                mur => mur.GetAllAsync(It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<MagnitudeUnit, bool>>>()), Times.Once,
+                "magnitudeUnitRepository GetAllAsync() is failed");
+            _eventCategoryRepositoryMock.Verify(ecr => ecr.AddAsync(It.Is<EventCategory>(ec => ec.Id == result), It.IsAny<CancellationToken>()),
+                Times.Once, "eventCategoryRepository AddAsync() is failed");
+            _unitOfWorkMock.Verify(u => u.SaveAsync(It.IsAny<CancellationToken>()), Times.Once, "unitOfWork SaveAsync() is failed");
+            result.Should().BeGreaterOrEqualTo(0);
+        }
+
+        [Test]
+        public async Task CreateEventCategoryHandlerTests_WhenUndefinedMagnitudeUnitIsNotExists_ShouldThrowNotFoundException()
+        {
+            // Arrange
+            var request = new CreateEventCategoryRequest()
+            {
+                CreateEventCategoryDto = new CreateEventCategoryDto { CategoryName = "Test" },
+            };
+
+            _eventCategoryRepositoryMock.Setup(x => x.AddAsync(It.IsAny<EventCategory>(), It.IsAny<CancellationToken>()));
+            _magnitudeUnitRepositoryMock.Setup(x =>
+                    x.GetAllAsync(It.IsAny<CancellationToken>(), mu => mu.MagnitudeUnitName == EntityNamesByDefault.DefaultMagnitudeUnit))
+                .ReturnsAsync(new List<MagnitudeUnit>());
+
+            _unitOfWorkMock.Setup(x => x.MagnitudeUnitRepository).Returns(_magnitudeUnitRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.EventCategoryRepository).Returns(_eventCategoryRepositoryMock.Object);
+
+            var handler = new CreateEventCategoryHandler(_unitOfWorkMock.Object);
+
+            // Act
+            Func<Task> act = async () => await handler.Handle(request, default);
+
+            // Assert
+            await act.Should().ThrowAsync<NotFoundException>().WithMessage("Entity - MagnitudeUnit(With name undefined) not found");
+            _magnitudeUnitRepositoryMock.Verify(
+                mur => mur.GetAllAsync(It.IsAny<CancellationToken>(), It.IsAny<Expression<Func<MagnitudeUnit, bool>>>()), Times.Once,
+                "magnitudeUnitRepository GetAllAsync() is failed");
+            _eventCategoryRepositoryMock.Verify(
+                ecr => ecr.AddAsync(It.Is<EventCategory>(ec => ec.CategoryName == request.CreateEventCategoryDto.CategoryName),
+                    It.IsAny<CancellationToken>()), Times.Never, "eventCategoryRepository AddAsync() is calls");
+            _unitOfWorkMock.Verify(u => u.SaveAsync(It.IsAny<CancellationToken>()), Times.Never, "unitOfWork SaveAsync() is calls");
         }
     }
 }
