bbe4337dc886ad38d742352ea501e5a85b65c68b
Roman Yeliukhin <ellowa123321@gmail.com>
2023-11-18 18:41:23 +0200

    Add BaseIntegrationTest

---
+++ InteractiveNaturalDisasterMap.Applications.IntegrationTests/Helpers/BaseIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using InteractiveNaturalDisasterMap.DataAccess.PostgreSql;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InteractiveNaturalDisasterMap.Applications.IntegrationTests.Helpers
+{
+    public class BaseIntegrationTest
+    {
+        private IntegrationTestsWebAppFactory _factory;
+        protected InteractiveNaturalDisasterMapDbContext DbContext;
+        protected IMediator Mediator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new IntegrationTestsWebAppFactory();
+            var scope = _factory.Services.CreateScope();
+            DbContext = scope.ServiceProvider.GetRequiredService<InteractiveNaturalDisasterMapDbContext>();
+            Mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _factory.Dispose();
+        }
+
+    }
+}
