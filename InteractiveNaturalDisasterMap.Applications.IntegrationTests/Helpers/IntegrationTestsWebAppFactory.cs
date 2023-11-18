bf7251f410be2d2e00d196fa3ac446a462dadd80
Roman Yeliukhin <ellowa123321@gmail.com>
2023-11-18 16:42:48 +0200

    Add IntegrationTestsWebAppFactory

---
+++ InteractiveNaturalDisasterMap.Applications.IntegrationTests/Helpers/IntegrationTestsWebAppFactory.cs
@@ -0,0 +1,31 @@
+﻿using InteractiveNaturalDisasterMap.DataAccess.PostgreSql;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InteractiveNaturalDisasterMap.Applications.IntegrationTests.Helpers
+{
+    public class IntegrationTestsWebAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                         typeof(DbContextOptions<InteractiveNaturalDisasterMapDbContext>));
+                if (dbContextDescriptor != null) services.Remove(dbContextDescriptor);
+
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+                services.AddDbContext<InteractiveNaturalDisasterMapDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(databaseName: Guid.Empty.ToString());
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+            });
+        }
+    }
+}
